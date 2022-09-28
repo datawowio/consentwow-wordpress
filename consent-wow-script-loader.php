3868339be1dac89427e372ada7e57db7e5f72d83
jsice <w14chin@gmail.com>
2022-09-28 11:58:09 +0700

    feat: init plugin file

---
+++ consent-wow-script-loader.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Consent Wow Script Loader
+ *
+ * @package consent-wow-script-loader
+ *
+ * @wordpress-plugin
+ * Plugin Name: Consent Wow Script Loader
+ * Plugin URI:  https://github.com/datawowio/consentwow-wordpress
+ * Description: An easy way to manage consent on web pages.
+ * Version:     1.0.0
+ * Author:      Consent Wow
+ * Author URI:  https://consentwow.com/
+ * License:     GNU General Public License v3 or later
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+define( 'WP_CONSENTWOW_VERSION', '1.0.0' );
+define( 'WP_CONSENTWOW_SLUG', 'consentwow-settings' );
+define( 'WP_CONSENTWOW_FILE', __FILE__ );
