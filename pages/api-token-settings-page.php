b5cf40ec3071be5528e1c04dc5e5eb7de8d366fd
jsice <w14chin@gmail.com>
2022-09-29 00:49:42 +0700

    feat: display settings section for api token

--- pages/api-token-settings-page.php
+++ pages/api-token-settings-page.php
@@ -5,6 +5,13 @@
  * @package consent-wow-script-loader
  */
 ?>
-<div>
-	API Token Settings
+<div class="wrap">
+	<h1 class="wp-heading-inline">API Token Settings</h1>
+	<form action="options.php" method="post">
+		<?php
+		settings_fields( 'consentwow_options_group' );
+		do_settings_sections( WP_CONSENTWOW_SLUG );
+		submit_button();
+		?>
+	</form>
 </div>
