42c5c31e838c311fdf4e6079bbedcd2a152fc3f2
Napatchol Thaipanich <patorseing@gmail.com>
2022-10-03 18:09:55 +0700

    chore: adjust form

--- pages/form-page.php
+++ pages/form-page.php
@@ -25,113 +25,121 @@ if (!isset($form)) {
 
 ?>
 <div class="wrap">
-	<h1 class="wp-heading-inline"><?php echo $title ?></h1>
-	<h4>ปลั๊กอินนี้สนับสนุนเฉพาะแบบฟอร์มประเภท Contact Form (CF7) คุณสามารถอ่านวิธีการติดตั้งและรายละเอียดการเพิ่มแบบฟอร์มได้ ที่นี่ </h4>
-	<form action="<?php echo admin_url('admin.php?action=consentwow_form_post'); ?>" method="post">
-		<?php consentwow_form_display_notice(); ?>
-		<?php if ($action == 'edit') : ?>
-			<input type="hidden" name="action" value="consentwow_form_post" />
-			<input name="consentwow_form[id]" type="hidden" value="<?php echo $id; ?>" />
-		<?php endif; ?>
-		<table class="form-table" role="presentation">
-			<tbody>
-				<tr class="consentwow-form-name-input">
-					<th scope="row">
-						<label for="consentwow_form_name">ชื่อฟอร์ม</label>
-					</th>
-					<td>
-						<input required type="text" id="consentwow_form_name" name="consentwow_form[form_name]" class="regular-text" value="<?php echo isset($form['form_name']) ? $form['form_name'] : ''; ?>" />
-					</td>
-				</tr>
-				<tr class="consentwow-form-id-input">
-					<th scope="row">
-						<label for="consentwow_form_id">ฟอร์ม ID</label>
-					</th>
-					<td>
-						<input required type="text" id="consentwow_form_id" name="consentwow_form[form_id]" class="regular-text" value="<?php echo isset($form['form_id']) ? $form['form_id'] : ''; ?>" />
-					</td>
-				</tr>
-				<tr class="consentwow-consent-input">
-					<th>เชื่อมกับข้อมูลของเจ้าของข้อมูล</th>
-				</tr>
-				<tr class="consentwow-email-input">
-					<th scope="row">
-						<label for="consentwow_email">Email (UID)</label>
-					</th>
-					<td>
-						<input required type="text" id="consentwow_email" name="consentwow_form[email]" class="regular-text" value="<?php echo isset($form['email']) ? $form['email'] : ''; ?>" />
-					</td>
-				</tr>
-				<tr class="consentwow-first-name-input">
-					<th scope="row">
-						<label for="consentwow_first_name">ชื่อจริง</label>
-					</th>
-					<td>
-						<input type="text" id="consentwow_first_name" name="consentwow_form[first_name]" class="regular-text" value="<?php echo isset($form['first_name']) ? $form['first_name'] : ''; ?>" />
-					</td>
-				</tr>
-				<tr class="consentwow-last-name-input">
-					<th scope="row">
-						<label for="consentwow_last_name">นามสกุล</label>
-					</th>
-					<td>
-						<input type="text" id="consentwow_last_name" name="consentwow_form[last_name]" class="regular-text" value="<?php echo isset($form['last_name']) ? $form['last_name'] : ''; ?>" />
-					</td>
-				</tr>
-				<tr class="consentwow-phone-number-input">
-					<th scope="row">
-						<label for="consentwow_phone_number">เบอร์โทรศัพท์</label>
-					</th>
-					<td>
-						<input type="text" id="consentwow_phone_number" name="consentwow_form[phone_number]" class="regular-text" value="<?php echo isset($form['phone_number']) ? $form['phone_number'] : ''; ?>" />
-					</td>
-				</tr>
-				<tr class="consentwow-consent-input">
-					<td>
-						<button class="button button-primary" style="width:fit-content;" onClick="addConsent()"> + Add Consent </button>
-					</td>
-				</tr>
-			</tbody>
-		</table>
-		<p class="submit">
-			<input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
-		</p>
-	</form>
+    <h1 class="wp-heading-inline"><?php echo $title ?></h1>
+    <h4>ปลั๊กอินนี้สนับสนุนเฉพาะแบบฟอร์มประเภท Contact Form (CF7)
+        คุณสามารถอ่านวิธีการติดตั้งและรายละเอียดการเพิ่มแบบฟอร์มได้ ที่นี่ </h4>
+    <form action="<?php echo admin_url('admin.php?action=consentwow_form_post'); ?>" method="post">
+        <?php consentwow_form_display_notice(); ?>
+        <?php if ($action == 'edit') : ?>
+        <input type="hidden" name="action" value="consentwow_form_post" />
+        <input name="consentwow_form[id]" type="hidden" value="<?php echo $id; ?>" />
+        <?php endif; ?>
+        <table class="form-table" role="presentation">
+            <tbody>
+                <tr class="consentwow-form-name-input">
+                    <th scope="row">
+                        <label for="consentwow_form_name">ชื่อฟอร์ม</label>
+                    </th>
+                    <td>
+                        <input required type="text" id="consentwow_form_name" name="consentwow_form[form_name]"
+                            placeholder="Contact form" class="regular-text"
+                            value="<?php echo isset($form['form_name']) ? $form['form_name'] : ''; ?>" />
+                    </td>
+                </tr>
+                <tr class="consentwow-form-id-input">
+                    <th scope="row">
+                        <label for="consentwow_form_id">ฟอร์ม ID</label>
+                    </th>
+                    <td>
+                        <input required type="text" id="consentwow_form_id" name="consentwow_form[form_id]"
+                            placeholder="1" class="regular-text"
+                            value="<?php echo isset($form['form_id']) ? $form['form_id'] : ''; ?>" />
+                    </td>
+                </tr>
+                <tr class="consentwow-consent-input">
+                    <th>เชื่อมกับข้อมูลของเจ้าของข้อมูล</th>
+                </tr>
+                <tr class="consentwow-email-input">
+                    <th scope="row">
+                        <label for="consentwow_email">Email (UID)</label>
+                    </th>
+                    <td>
+                        <input required type="text" id="consentwow_email" name="consentwow_form[email]"
+                            placeholder="อีเมลแอดเดรส" class="regular-text"
+                            value="<?php echo isset($form['email']) ? $form['email'] : ''; ?>" />
+                    </td>
+                </tr>
+                <tr class="consentwow-first-name-input">
+                    <th scope="row">
+                        <label for="consentwow_first_name">ชื่อจริง</label>
+                    </th>
+                    <td>
+                        <input type="text" id="consentwow_first_name" name="consentwow_form[first_name]"
+                            placeholder="ชื่อจริง" class="regular-text"
+                            value="<?php echo isset($form['first_name']) ? $form['first_name'] : ''; ?>" />
+                    </td>
+                </tr>
+                <tr class="consentwow-last-name-input">
+                    <th scope="row">
+                        <label for="consentwow_last_name">นามสกุล</label>
+                    </th>
+                    <td>
+                        <input type="text" id="consentwow_last_name" name="consentwow_form[last_name]"
+                            placeholder="นามสกุล" class="regular-text"
+                            value="<?php echo isset($form['last_name']) ? $form['last_name'] : ''; ?>" />
+                    </td>
+                </tr>
+                <tr class="consentwow-phone-number-input">
+                    <th scope="row">
+                        <label for="consentwow_phone_number">เบอร์โทรศัพท์</label>
+                    </th>
+                    <td>
+                        <input type="text" id="consentwow_phone_number" name="consentwow_form[phone_number]"
+                            placeholder="เบอร์โทรศัพท์" class="regular-text"
+                            value="<?php echo isset($form['phone_number']) ? $form['phone_number'] : ''; ?>" />
+                    </td>
+                </tr>
+                <tr class="consentwow-consent-head">
+                    <th>
+                        เชื่อมกับข้อมูลความยินยอม
+                    </th>
+                </tr>
+            </tbody>
+        </table>
+        <div class="submit">
+            <button class="button button-primary" style="width:fit-content;" onClick="addConsent()"> +
+                เพิ่มการเชื่อมต่อกับข้อมูลความยินยอม </button>
+        </div>
+        <div class="submit">
+            <input type="submit" name="submit" id="submit" class="button button-primary" value="เพิ่มฟอร์ม">
+        </div>
+    </form>
 
-	<script>
-		let i = 0;
-		const addConsent = () => {
-			const contentTable = document.getElementsByTagName('tbody');
-			if (i === 0) {
-				const headTag = document.createElement('tr');
-				const th = document.createElement('th');
-				headTag.classList.add('consentwow-consent-head')
-				const text = document.createTextNode("เชื่อมกับข้อมูลความยินยอม")
-				th.appendChild(text)
-				headTag.appendChild(th)
-				contentTable[0].appendChild(headTag)
-			}
-			const tag = document.createElement('tr');
-			tag.classList.add('consentwow-consent-input');
-			const purposeKeyCol = document.createElement('td');
-			const purposeKeyInput = document.createElement("input");
-			purposeKeyInput.type = "text";
-            purposeKeyInput.name = `consentwow_form[conset_key_${i}]`;
-			purposeKeyInput.classList.add('regular-text')
-			purposeKeyInput.placeholder = "ID ของวัตถุประสงค์"
-			purposeKeyInput.style = "width:200px;"
-			purposeKeyCol.appendChild(purposeKeyInput)
-			tag.appendChild(purposeKeyCol)
-			const purposeNameCol = document.createElement('td');
-			const purposeNameInput = document.createElement("input");
-			purposeNameInput.type = "text";
-            purposeNameInput.name = `consentwow_form[conset_name_${i}]`;
-			purposeNameInput.classList.add('regular-text')
-			purposeNameInput.placeholder = "ชื่อวัตุประสงค์ความยินยอม"
-			purposeNameCol.appendChild(purposeNameInput)
-			tag.appendChild(purposeNameCol)
-			contentTable[0].appendChild(tag)
-			i += 1;
-		}
-	</script>
+    <script>
+    let i = 0;
+    const addConsent = () => {
+        const contentTable = document.getElementsByTagName('tbody');
+        const tag = document.createElement('tr');
+        tag.classList.add('consentwow-consent-input');
+        const purposeKeyCol = document.createElement('td');
+        const purposeKeyInput = document.createElement("input");
+        purposeKeyInput.type = "text";
+        purposeKeyInput.name = `consentwow_form[conset_key_${i}]`;
+        purposeKeyInput.classList.add('regular-text')
+        purposeKeyInput.placeholder = "ID ของวัตถุประสงค์"
+        purposeKeyInput.style = "width:200px;"
+        purposeKeyCol.appendChild(purposeKeyInput)
+        tag.appendChild(purposeKeyCol)
+        const purposeNameCol = document.createElement('td');
+        const purposeNameInput = document.createElement("input");
+        purposeNameInput.type = "text";
+        purposeNameInput.name = `consentwow_form[conset_name_${i}]`;
+        purposeNameInput.classList.add('regular-text')
+        purposeNameInput.placeholder = "ชื่อวัตุประสงค์ความยินยอม"
+        purposeNameCol.appendChild(purposeNameInput)
+        tag.appendChild(purposeNameCol)
+        contentTable[0].appendChild(tag)
+        i += 1;
+    }
+    </script>
 </div>
